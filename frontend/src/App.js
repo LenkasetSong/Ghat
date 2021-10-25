35d489f1c7a388aa6686fa14d9aa662d15c9fff5
4thrun <65476356+4thrun@users.noreply.github.com>
2021-10-25 18:24:28 +0800

    2021-10-25 18:24(UTC/GMT+08:00) Header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,9 @@ import React, {Component} from "react";
 import "./App.css";
 import { connect, sendMsg } from "./api";
 
+import Header from "./components/Header/Header";
+
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -17,6 +20,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header />
         <button onClick={this.send}>Click</button>
       </div>
     );
